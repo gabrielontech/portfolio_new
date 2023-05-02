564e177d5f27d7d09c0dda270ef04e48c00540a6
Gabriel OnTech <70663867+gabrielontech@users.noreply.github.com>
2023-05-02 14:21:52 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -960,9 +960,9 @@ export default function Home({ publications }) {
               />
               {/* Project Three */}
               <FeaturedProjectCard
-                title={"Gsb Expenses Tacker Android App"}
+                title={"Okoto Mobile App"}
                 status={"Full-stack project"}
-                description={`The GSB application is currently accessible through a classic web application. The accountants being present on the site of the company have access to it by their workstation but the visitors in addition to the products they carry must have a laptop. In order to lighten their equipment, I was asked to develop an Android application with which visitors can with which visitors can register their expenses and consult them.`}
+                description={`In the first part of our complete e-commerce app, we show you how you can create a nice clean onboarding screen for your e-commerce app that can run both Andriod and iOS devices because it builds with flutter. Then on the second episode, we build a Sign in, Forgot Password screen with a custom error indicator. The third Episode contains Sign Up and OTP screen. The fourth episode is the most exciting episode because we build our home page on this episode than on the fifth episode we work on our product page, last not least on the 6th episode we build an order screen which has some cool stuff like swipe to remove. At the end 7th episode we build a profile screen.`}
                 float={`right-0`}
                 flexDirection={`flex-col lg:flex-row`}
                 imgWidth={"1366"}
