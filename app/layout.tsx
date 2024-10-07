322e548a070527349dad02dd1d57534d516553e4
gabriel kitoko <gaabrielkeller19@gmail.com>
2024-10-07 15:34:55 +0200

    finish

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = localFont({
